e5967002a11f5b820ea8bd615e8d3fc35aaca84d
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-19 14:23:10 +0200

    creates parse request method that fetch request components

--- src/main/java/HttpRequest.java
+++ src/main/java/HttpRequest.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,7 +10,7 @@ public class HttpRequest {
     private String body;
 
     public HttpRequest() {
-         this.headers = new HashMap<>();
+        this.headers = new HashMap<>();
     }
 
     public String getMethod() {
@@ -47,8 +48,41 @@ public class HttpRequest {
     public Map<String, String> getHeaders() {
         return headers;
     }
-    public void addHeader(String key, String value)
-    {
+
+    public void addHeader(String key, String value) {
         headers.put(key, value);
     }
+
+    public HttpRequest parseRequest(String line) {
+        // fetch request components based on crlf \r\n (request line,headers,body)
+        HttpRequest parsedRequest = new HttpRequest();
+
+        String[] tokens = line.split("\r\n", -1);
+
+        String requestLine = tokens[0].stripLeading();
+        parsedRequest.parseRequestLine(requestLine);
+
+        int index = 1;
+        while(index <tokens.length && !tokens[index].isEmpty())
+        {
+            String header = tokens[index].strip();
+            String[] headerTokens = header.split(":", 2);
+            if(headerTokens.length == 2)
+            {
+                parsedRequest.addHeader(headerTokens[0], headerTokens[1]);
+            }
+            ++index;
+        }
+
+        if(index +1 < tokens.length)
+        {
+            parsedRequest.setBody(String.join("\r\n", Arrays.copyOfRange(tokens,index+1,tokens.length)));
+        }
+        else
+            parsedRequest.setBody("");
+
+        return parsedRequest;
+
+    }
+
 }
