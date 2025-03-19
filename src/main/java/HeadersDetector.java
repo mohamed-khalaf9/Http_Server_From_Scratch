e857a600e9e185e8ac029600b049838f23793a8e
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-19 23:59:29 +0200

    implement method to detect range header and validate range syntax correctness

--- src/main/java/HeadersDetector.java
+++ src/main/java/HeadersDetector.java
@@ -31,11 +31,47 @@ public class HeadersDetector {
     }
     public boolean detectAcceptEncoding(Map<String, String> headers)
     {
+        if(headers.containsKey("Accept-Encoding"))
+        {
+            String headerValue = headers.get("Accept-Encoding");
+            if(headerValue.equalsIgnoreCase("gzip"))
+                return true;
+            else
+                return false;
+        }
+        return false;
 
     }
 
     public boolean detectRange(Map<String, String> headers)
     {
+        if(headers.containsKey("Range"))
+        {
+            String headerValue = headers.get("Range");
+            headerValue = headerValue.strip();
+            String rangePart = headerValue.substring(6);
+            String[] parts = rangePart.split("-");
+            if(parts.length!=2)
+                return false;
+
+            try{
+                if(!parts[0].isEmpty())
+                {
+                    Integer.parseInt(parts[0]);
+                }
+                if(!parts[1].isEmpty())
+                {
+                    Integer.parseInt(parts[1]);
+                }
+                return true;
+            }
+            catch(NumberFormatException e) {
+                return false;
+            }
+
+        }
+
+        return false;
 
     }
 }
