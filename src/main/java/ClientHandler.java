84e32f0dd21d34e49833a0f59220824b9ed93c60
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-19 15:19:13 +0200

    creates socket data member which is clientsocket, creates constructor,creates run method which initalize i/o streams and start accepting requests

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -1,2 +1,34 @@
-public class ClientHandler {
+import java.io.*;
+import java.net.Socket;
+
+public class ClientHandler implements  Runnable{
+    private Socket socket;
+
+    ClientHandler(Socket socket)
+    {
+        this.socket = socket;
+    }
+    @Override
+    public void run() {
+        System.out.println("hellow world");
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+
+            String request;
+            while ((request = in.readLine()) != null) {
+                System.out.println(request);
+                out.println(request);
+                out.flush();
+            }
+
+
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 }
