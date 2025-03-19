b72cfb91082dd1f5f4f82a6f502ba46acdce9ef1
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-20 00:11:46 +0200

    declare http response data members

--- src/main/java/HttpResponse.java
+++ src/main/java/HttpResponse.java
@@ -1,2 +1,11 @@
+import java.util.Map;
+
 public class HttpResponse {
+    private String version;
+    private int statusCode;
+    private int statusText;
+    private Map<String, String> headers;
+    private String body;
+
+
 }
