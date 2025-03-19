c7dcd0323266451f3797225ac16eeddb2507b1bf
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-19 14:37:15 +0200

    implements start method that define server to some port and start accepting client connections , once client connected handling it in a new thread, so I am now handle concurrent connnections

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -1,4 +1,8 @@
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
     public int port;
@@ -6,6 +10,17 @@ public class HttpServer {
 
     public HttpServer(int port) {
         this.port = port;
+    }
+
+    public void start() throws IOException {
+        serverSocket = new ServerSocket(port);
+        System.out.println("Server started on port " + port);
+        while (true){
+            Socket socket = serverSocket.accept();
+            ClientHandler clientHandler = new ClientHandler(socket);
+            ExecutorService executor = Executors.newCachedThreadPool();
+            executor.execute(clientHandler);
+        }
     }
 
 
