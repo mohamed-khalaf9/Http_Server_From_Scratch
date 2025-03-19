9cecfdfe8d7c3e9eeae628044fbb42d2c9365a98
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-19 16:56:19 +0200

    implment get handler method to return the appropraite handler based on the method and the target

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 
@@ -14,6 +15,18 @@ public class Router {
     public void addRoute(String method, String path, Function<HttpRequest, HttpResponse> handler) {
         routes.computeIfAbsent(method, k -> new ConcurrentHashMap<>())
                 .put(path, handler);
+    }
+
+    public Optional<Function<HttpRequest,HttpResponse>> getHandler(String method, String target)
+    {
+        Map<String,Function<HttpRequest,HttpResponse>> handlers = routes.get(method);
+        if(handlers != null)
+        {
+            return Optional.ofNullable(handlers.get(target));
+        }
+
+        return Optional.empty();
+
     }
 
 
