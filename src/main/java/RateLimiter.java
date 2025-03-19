e5e7b37b54702a474b163ae9504877eb639ba149
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-19 15:51:47 +0200

    define the constructor

--- src/main/java/RateLimiter.java
+++ src/main/java/RateLimiter.java
@@ -1,9 +1,17 @@
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class RateLimiter {
     private int maxRequests;
     private long maxWindowSize;
     private Queue<Long> requestslog;
+
+
+    public RateLimiter(int maxRequests, long maxWindowSize) {
+        this.maxRequests = maxRequests;
+        this.maxWindowSize = maxWindowSize;
+        this.requestslog = new LinkedList<Long>();
+    }
 
 
 }
