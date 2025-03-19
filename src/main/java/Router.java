7a3e81ef5f1a59559f7ff67d09a7c442f6495303
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-19 16:35:27 +0200

    define map that associate each http method to map of target and handler method

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -1,2 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+
 public class Router {
+    public Map<String,Map<String, Function<HttpRequest,HttpResponse>>> routes;
+
+    public Router()
+    {
+        routes = new ConcurrentHashMap<>();
+    }
+
+
+
 }
