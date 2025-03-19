8d62e6bb7c2517c8297e0366fcc448bd41158598
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-20 00:14:49 +0200

    define getters and setters for http response components

--- src/main/java/HttpResponse.java
+++ src/main/java/HttpResponse.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class HttpResponse {
@@ -7,5 +8,48 @@ public class HttpResponse {
     private Map<String, String> headers;
     private String body;
 
+    public HttpResponse()
+    {
+        this.setVersion("Http1.1");
+        this.headers = new HashMap<String, String>();
+    }
 
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public int getStatusText() {
+        return statusText;
+    }
+
+    public void setStatusText(int statusText) {
+        this.statusText = statusText;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void addHeader(String key, String value) {
+        this.headers.put(key, value);
+    }
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
 }
