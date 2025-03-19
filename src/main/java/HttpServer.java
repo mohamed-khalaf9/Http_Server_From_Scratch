4f926f0e5432c964c133a5e4a560264133d7149b
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-19 14:30:00 +0200

    define port and socket and constructor that define port number

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -1,4 +1,12 @@
+import java.net.ServerSocket;
+
 public class HttpServer {
+    public int port;
+    public ServerSocket serverSocket;
+
+    public HttpServer(int port) {
+        this.port = port;
+    }
 
 
 }
