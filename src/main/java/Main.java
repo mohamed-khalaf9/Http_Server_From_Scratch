27c79cc9da4e2492846ea880bd7c7964cd8e99fb
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-21 16:27:26 +0200

    define logging feature calsses

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
+import java.io.IOException;
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
+     HttpServer server = new HttpServer(45321);
+     try{
+         server.start();
+     }
+     catch(IOException e){
+         e.printStackTrace();
+     }
+
 
     }
 }
