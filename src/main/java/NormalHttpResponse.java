43dd5b82ce270c672444316b96262efe149e4fc7
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-21 22:47:23 +0200

    extend httpresponse class to provide http response that has body type compressed or not

--- src/main/java/NormalHttpResponse.java
+++ src/main/java/NormalHttpResponse.java
@@ -1,98 +1,33 @@
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
-public class HttpResponse {
-    private String version;
-    private int statusCode;
-    private String statusText;
-    private Map<String, String> headers;
-    private byte[] body;
+public class NormalHttpResponse extends HttpResponse {
+    private String body;
 
-
-    public HttpResponse()
-    {
-        this.setVersion("Http1.1");
-        this.headers = new HashMap<String, String>();
+    public NormalHttpResponse() {
+        super();
     }
 
-    public String getVersion() {
-        return version;
+    public String getBody() {
+        return body;
     }
 
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    public int getStatusCode() {
-        return statusCode;
-    }
-
-    public void setStatusCode(int statusCode) {
-        this.statusCode = statusCode;
-    }
-
-    public String getStatusText() {
-        return statusText;
-    }
-
-    public void setStatusText(String statusText) {
-        this.statusText = statusText;
-    }
-
-    public byte[] getBody() {
-        return this.body;
-    }
-
-    public void setBodyString(String body)
-    {
-        this.bodyString = body;
-        this.headers.put("Content-Length", String.valueOf(this.bodyString.length()));
-        this.headers.put("Content-Type", "text/plain; charset=UTF-8");
-
-    }
-    public String getBodyString(){
-        return this.bodyString;
-    }
-
-
-    public void addHeader(String key, String value) {
-        this.headers.put(key, value);
-    }
-    public Map<String, String> getHeaders() {
-        return headers;
-    }
-
-    @Override
-    public String toString() {
-        return "HttpResponse{" +
-                "version='" + version + '\'' +
-                ", statusCode=" + statusCode +
-                ", statusText='" + statusText + '\'' +
-                ", headers=" + headers +
-                ", bodyBytes=" + Arrays.toString(bodyBytes) +
-                ", bodyString='" + bodyString + '\'' +
-                '}';
+    public void setBody(String body) {
+        this.body = body;
     }
 
     public String constructResponseFormat() {
         StringBuilder response = new StringBuilder();
-        response.append(this.version)
+        response.append(super.getVersion())
                 .append(" ")
-                .append(this.statusCode)
+                .append(super.getStatusCode())
                 .append(" ")
-                .append(this.statusText)
+                .append(super.getStatusText())
                 .append("\r\n");
-        for (Map.Entry<String, String> entry : headers.entrySet()) {
+        for (Map.Entry<String, String> entry : super.getHeaders().entrySet()) {
             response.append(entry.getKey()).append(": ").append(entry.getValue()).append("\r\n");
         }
         response.append("\r\n");
-        response.append(Objects.toString(this.bodyString, "")).append("\r\n");
-
+        response.append(body);
         return response.toString();
     }
-
 }
-
