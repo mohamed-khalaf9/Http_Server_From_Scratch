e513c0f664451817ad04b9f5cbf3d47f216d3dd5
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-23 01:29:24 +0200

    add router data memebr to be shared among all threads

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -7,9 +7,11 @@ import java.util.concurrent.Executors;
 public class HttpServer {
     public int port;
     public ServerSocket serverSocket;
+    public Router router;
 
-    public HttpServer(int port) {
+    public HttpServer(int port,Router router) {
         this.port = port;
+        this.router = router;
     }
 
     public void start() throws IOException {
@@ -17,7 +19,7 @@ public class HttpServer {
         System.out.println("Server started on port " + port);
         while (true){
             Socket socket = serverSocket.accept();
-            ClientHandler clientHandler = new ClientHandler(socket);
+            ClientHandler clientHandler = new ClientHandler(socket,router);
             ExecutorService executor = Executors.newCachedThreadPool();
             executor.execute(clientHandler);
         }
