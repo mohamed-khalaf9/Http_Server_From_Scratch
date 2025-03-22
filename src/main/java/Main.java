00aecf0b59f14968fe50b509634b7b20ee7b3205
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-23 01:30:53 +0200

    define routes,start the server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,26 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-     HttpServer server = new HttpServer(45321);
-     try{
-         server.start();
-     }
-     catch(IOException e){
-         e.printStackTrace();
-     }
+        Router router = new Router();
+        Handlers handlers = new Handlers();
+
+        // Register routes
+        router.addRoute("POST", "/create", handlers::createFileHandler);
+        router.addRoute("GET", "/file", handlers::getFileHandler);
+        HttpServer server = new HttpServer(45321,router);
 
 
+
+
+        try {
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+
+
 }
+
+
