2696e81128b0c80b6bb25ef2bbb892a1b0df6bdd
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-22 14:18:26 +0200

    refactor to solve bug

--- src/main/java/HttpResponse.java
+++ src/main/java/HttpResponse.java
@@ -8,6 +8,7 @@ public class HttpResponse {
     private int statusCode;
     private String statusText;
     private Map<String, String> headers;
+    private String body;
 
 
     public HttpResponse()
@@ -45,6 +46,24 @@ public class HttpResponse {
     }
     public Map<String, String> getHeaders() {
         return headers;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+    public String getRawResponse(){
+        StringBuilder response = new StringBuilder();
+        response.append("HTTP/1.1 ").append(statusCode).append(" ").append(statusText).append("\r\n");
+        for(Map.Entry<String, String> header : headers.entrySet()){
+            response.append(header.getKey()).append(": ").append(header.getValue()).append("\r\n");
+        }
+        response.append("\r\n");
+        response.append(body);
+        return response.toString();
     }
 
 }
