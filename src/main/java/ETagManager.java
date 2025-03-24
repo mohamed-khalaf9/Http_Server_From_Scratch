5ba37ef01de9ceb059154a411292e9305f013388
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-24 21:27:17 +0200

    refactor the method and make it just generate e tag not store one

--- src/main/java/ETagManager.java
+++ src/main/java/ETagManager.java
@@ -1,6 +1,8 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.ECField;
@@ -26,32 +28,21 @@ public class ETagManager {
     }
 
 
-    public synchronized String generateEtag(String filePath) {
-        try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            FileInputStream fis = new FileInputStream(filePath);
-
-            byte[] buffer = new byte[4192];
-            int bytesRead;
-
-            while ((bytesRead = fis.read(buffer)) != -1) {
-                digest.update(buffer, 0, (int) bytesRead);
-            }
-            fis.close();
-            byte[] hash = digest.digest();
-            String etag = Base64.getEncoder().encodeToString(hash);
-            fileEtags.put(filePath, etag);
-            return etag;
+    public synchronized String generateETag(String fileName) throws IOException, NoSuchAlgorithmException {
+        Path filePath = Path.of(fileName);
+        if (!Files.exists(filePath)) {
+            throw new IOException("File not found: " + fileName);
         }
-        catch (Exception e)
-        {
-            return null;
-        }
+
+        byte[] fileContent = Files.readAllBytes(filePath);
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(fileContent);
+        return Base64.getEncoder().encodeToString(hash);
     }
 
     public synchronized boolean compare(String filePath,String etag) {
         try {
-            String currentEtag = this.generateEtag(filePath);
+            String currentEtag = this.generateETag(filePath);
             return currentEtag.equals(etag);
         }
         catch (Exception c)
