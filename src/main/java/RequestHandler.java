bd0f37388a5fac6df669cff094c252164f1da1a8
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-25 01:50:59 +0200

    define constructor of the class , accept http request to access request components,request log to log the request as well it has ip addresse of the client, router to share routes, client handler to decrement the active requests after serving the request,and out to flush the response to the client

--- src/main/java/RequestHandler.java
+++ src/main/java/RequestHandler.java
@@ -1,7 +1,47 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.function.Function;
+
 public class RequestHandler implements Runnable{
+    private HttpRequest request;
+    private HttpRequestLog requestLog;
+    private Router router;
+    private BufferedWriter out;
+    private ClientHandler clientHandler;
+
+
+    public RequestHandler(HttpRequest request, HttpRequestLog requestLog, Router router, ClientHandler clientHandler, BufferedWriter out) {
+        this.request = request;
+        this.requestLog = requestLog;
+        this.router = router;
+        this.clientHandler = clientHandler;
+        this.out = out;
+    }
+
+
+
+
+
 
     @Override
     public void run() {
+        Function<HttpRequest, HttpResponse> handler = router.getHandler(request.getMethod(), request.getTarget());
+
+        try {
+            if (handler != null) {
+                HttpResponse response = handler.apply(request);
+                sendResponse(out, response); // Send the response back to the client
+            } else {
+                HttpResponse response = new HttpResponse();
+                response.setStatusCode(404);
+                response.setStatusText("Handler Not Found");
+                sendResponse(out, response);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
 
     }
 }
