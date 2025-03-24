f4fa45bb4e93e97f1924b106251b5bf9092b9900
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-24 02:04:35 +0200

    solve the bug of parsing body

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -74,7 +74,7 @@ public class ClientHandler implements  Runnable{
         String target = requestLineParts[1];
         String[] targetParts = target.split("/");
 
-        String requestTarget = "/"+targetParts[1].trim();// e.g., "/create/example.txt"
+        String requestTarget = "/"+targetParts[1];// e.g., "/create/example.txt"
         String pathParameter = targetParts[2];
 
         // Read headers (ignore body)
@@ -90,9 +90,28 @@ public class ClientHandler implements  Runnable{
         }
 
         // Extract the path parameter (e.g., "example.txt")
+        String body = null;
+        if (headers.containsKey("Content-Length")) {
+            int contentLength = Integer.parseInt(headers.get("Content-Length").trim());
+            char[] bodyChars = new char[contentLength];
+            int totalRead = 0;
+            while (totalRead < contentLength) {
+                int readBytes = bufferedReader.read(bodyChars, totalRead, contentLength - totalRead);
+                if (readBytes == -1) {
+                    System.out.println("End of stream reached before reading all content.");
+                    break;
+                }
+                totalRead += readBytes;
+            }
+            if (totalRead == contentLength) {
+                body = new String(bodyChars);
+            } else {
+                System.out.printf("Incomplete body received! Expected %d, got %d%n", contentLength, totalRead);
+            }
+        }
 
 
-        return new HttpRequest(method, requestTarget, headers, pathParameter);
+        return new HttpRequest(method, requestTarget, headers, pathParameter,body);
     }
 
 
