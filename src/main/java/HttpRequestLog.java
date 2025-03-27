488e4d6e3d76886f2753a0e6f24bb0002e4be841
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-27 16:13:03 +0200

    define consturctor that accepts http request object,ip addresse of client and timestamp and internally define an ID for the request

--- src/main/java/HttpRequestLog.java
+++ src/main/java/HttpRequestLog.java
@@ -1,8 +1,17 @@
+import java.util.UUID;
+
 public class HttpRequestLog {
     private String requestId;
     private String ipAddresse;
     private long timestamp;
     private HttpRequest httpRequest;
+
+    public HttpRequestLog(HttpRequest httpRequest,String ipAddresse,long timestamp) {
+        this.requestId = UUID.randomUUID().toString();
+        this.ipAddresse = ipAddresse;
+        this.timestamp = timestamp;
+        this.httpRequest = httpRequest;
+    }
 
 
 
