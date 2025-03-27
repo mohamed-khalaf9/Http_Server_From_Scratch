cfb0a80a449433b4e1fa8aee4565b5f861b9ffbb
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-27 16:30:44 +0200

    define the logs file and gson which convert object to json representation

--- src/main/java/Logger.java
+++ src/main/java/Logger.java
@@ -1,2 +1,11 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class Logger {
+    private static final String LOG_FILE_PATH = "http_logs.json";
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+
+
+
 }
