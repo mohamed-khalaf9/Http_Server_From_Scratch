ad118785bed131ac4eacefa65e4edbff15c9aa14
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-27 16:32:09 +0200

    define write log to file method to convert object ot a json representation then write it to the file

--- src/main/java/Logger.java
+++ src/main/java/Logger.java
@@ -1,10 +1,26 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Logger {
     private static final String LOG_FILE_PATH = "http_logs.json";
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
+
+
+
+
+    private static synchronized void writeLogToFile(Object log) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(LOG_FILE_PATH, true))) {
+            String jsonLog = gson.toJson(log);
+            writer.println(jsonLog);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 
