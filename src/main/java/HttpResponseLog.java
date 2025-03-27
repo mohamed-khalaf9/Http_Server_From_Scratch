28a4cc73c443fce7ec84807504a35b97bca2a031
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-03-27 16:21:05 +0200

    define http response log components

--- src/main/java/HttpResponseLog.java
+++ src/main/java/HttpResponseLog.java
@@ -1,3 +1,20 @@
+import java.util.UUID;
+
 public class HttpResponseLog {
+    private String responseId;
+    private String requestId;
+    private String clientIpAddresse;
+    private long processingTime;
+    private HttpResponse httpResponse;
+    private int resoureceSize;
+
+    public HttpResponseLog(String requestId,String clientIpAddresse,long processingTime,HttpResponse httpResponse) {
+        this.responseId = UUID.randomUUID().toString();
+        this.requestId = requestId;
+        this.clientIpAddresse = clientIpAddresse;
+        this.processingTime = processingTime;
+        this.httpResponse = httpResponse;
+        this.resoureceSize = httpResponse.getBody() != null ? httpResponse.getBody().length():0;
+    }
 
 }
